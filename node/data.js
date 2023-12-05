947d1e5652f187a4c5e21e3bc48fe60918935899
oortmealy <oortmealy@gmail.com>
2023-12-05 13:31:55 +0900

    데모버전

--- node/data.js
+++ node/data.js
@@ -1,26 +1,3 @@
-// data.js
-/*window.onload = function () {
-  if (!getCookie("hidePopup")) {
-    document.getElementById("welcomePopup").style.display = "block";
-  }
-}
-
-function closePopup() {
-  document.getElementById("welcomePopup").style.display = "none";
-}
-
-function setCookieAndClosePopup() {
-  var date = new Date();
-  date.setTime(date.getTime() + (24 * 60 * 60 * 1000)); // 24시간
-  document.cookie = "hidePopup=true; expires=" + date.toUTCString();
-  closePopup();
-}
-
-function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length === 2) return parts.pop().split(";").shift();
-}*/
 
 function connectNodes(node1, node2, color = 'black', width = 2) {
   var x1 = node1.offsetLeft + node1.offsetWidth / 2;
@@ -68,4 +45,3 @@ connectNodes(document.getElementById('node7'), document.getElementById('node10')
 connectNodes(document.getElementById('node8'), document.getElementById('node9'), 'black', 4);
 
 connectNodes(document.getElementById('node9'), document.getElementById('node10'), 'black', 4);
-
